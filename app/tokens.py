9761a2822bf32f0502041ef8133b598c59fcfa72
Ritik Raj <ritik@air.local>
2024-12-18 16:21:55 +0530

    Add support for other characters

--- app/tokens.py
+++ app/tokens.py
@@ -3,6 +3,13 @@ TOKEN_MAP = {
     ")": "RIGHT_PAREN",
     "{": "LEFT_BRACE",
     "}": "RIGHT_BRACE",
+    "*": "STAR",
+    ".": "DOT",
+    ",": "COMMA",
+    "+": "PLUS",
+    "-": "MINUS",
+    ";": "SEMICOLON",
+    "/": "SLASH",
 }
 
 def get_token_type(token):
