03fb44787577f5efcf9c3d192c0dc65799082635
Ritik Raj <ritik@air.local>
2024-12-23 23:13:07 +0530

    Create a scanner class to scan input

--- app/main.py
+++ app/main.py
@@ -28,7 +28,6 @@ class TokenType(enum.Enum):
     NUMBER = "NUMBER"
     EOF = "EOF"
 
-
 class Token:
     def __init__(self, type: TokenType, lexeme: str, literal: Any | None, line: int) -> None:
         self.type = type
@@ -38,6 +37,110 @@ class Token:
 
     def __str__(self) -> str:
         return f"{self.type} {self.lexeme} {self.literal}"
+
+class Scanner:
+    def __init__(self, source: str) -> None:
+        self.source = source
+        self.tokens: list[Token] = []
+        self.start = 0
+        self.current = 0
+        self.line = 1
+        self.errors: list[str] = []
+    def scan_tokens(self) -> tuple[list[Token], list[str]]:
+        while not self.is_at_end():
+            self.start = self.current
+            self.scan_token()
+        self.tokens.append(Token(TokenType.EOF, "", None, self.line))
+        return self.tokens, self.errors
+
+    def is_at_end(self) -> bool:
+        return self.current >= len(self.source)
+
+    def scan_token(self) -> None:
+        char = self.advance()
+        match char:
+            case "(":
+                self.add_token(TokenType.LEFT_PAREN)
+            case ")":
+                self.add_token(TokenType.RIGHT_PAREN)
+            case "{":
+                self.add_token(TokenType.LEFT_BRACE)
+            case "}":
+                self.add_token(TokenType.RIGHT_BRACE)
+            case "*":
+                self.add_token(TokenType.STAR)
+            case ".":
+                self.add_token(TokenType.DOT)
+            case ",":
+                self.add_token(TokenType.COMMA)
+            case "+":
+                self.add_token(TokenType.PLUS)
+            case "-":
+                self.add_token(TokenType.MINUS)
+            case ";":
+                self.add_token(TokenType.SEMICOLON)
+            case "!":
+                self.add_token(TokenType.BANG_EQUAL if self.match("=") else TokenType.BANG)
+            case "=":
+                self.add_token(TokenType.EQUAL_EQUAL) if self.match("=") else self.add_token(TokenType.EQUAL)
+            case "<":
+                self.add_token(TokenType.LESS_EQUAL if self.match("=") else TokenType.LESS)
+            case ">":
+                self.add_token(TokenType.GREATER_EQUAL if self.match("=") else TokenType.GREATER)
+            case "/":
+                if self.match("/"):
+                    while self.peek() != "\n" and not self.is_at_end():
+                        self.advance()
+                else:
+                    self.add_token(TokenType.SLASH)
+            case " ", "\r", "\t":
+                pass
+            case "\n":
+                self.line += 1
+            case "\"":
+                self.string()
+            case _:
+                self.error(f"Unexpected character: {char}")
+
+    def advance(self) -> str:
+        self.current += 1
+        return self.source[self.current - 1]
+
+    def add_token(self, token_type: TokenType, literal: Any | None = None) -> None:
+        text = self.source[self.start:self.current]
+        self.tokens.append(Token(token_type, text, literal, self.line))
+
+    def peek(self) -> str:
+        if self.is_at_end():
+            return "\0"
+        return self.source[self.current]
+
+    def match(self, expected: str) -> bool:
+        if self.is_at_end():
+            return False
+        if self.source[self.current] != expected:
+            return False
+        self.current += 1
+        return True
+
+    def string(self) -> None:
+        while self.peek() != "\"" and not self.is_at_end():
+            if self.peek() == "\n":
+                self.line += 1
+            self.advance()
+
+        if self.is_at_end():
+            self.errors.append(f"[line {self.line}] Error: Unterminated string.")
+            return
+
+        self.advance()
+
+        value = self.source[self.start + 1:self.current - 1]
+        self.add_token(TokenType.STRING, value)
+
+    def error(self, message: str) -> None:
+        self.errors.append(f"[line {self.line}] Error: {message}")
+
 
 
 
